0ef998a1ae96a9dd5d7f61f476145580dbc3a731
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-15 09:40:02 +0600

    explore Math operation

---
+++ Math.js
@@ -0,0 +1,27 @@
+//* Math.pow()
+// console.log(Math.pow(5, 3))
+
+//* Math.abs();
+// console.log(Math.abs(-5)) //? it always returns positive value of a number
+
+//* Math.round()
+// console.log(Math.round(2.49))
+// console.log(Math.round(2.5))
+
+//* Math.ceil()
+// console.log(Math.ceil(5.09))
+
+//* Math.floor()
+// console.log(Math.floor(1.999999999))
+
+//* Math.random()
+// console.log(Math.ceil(Math.random() * 100))
+// for (let i = 0; i < 9; i++) {
+//     const random = Math.ceil(Math.random() * 100);
+//     console.log(random)
+// }
+
+//* Math.max()/min()
+const arr = [21, 52, 14, 62];
+// console.log(Math.max(...arr))
+// console.log(Math.min(...arr))
