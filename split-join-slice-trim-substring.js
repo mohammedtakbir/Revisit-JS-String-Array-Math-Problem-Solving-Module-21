3cf669baf9e89d699df4fb8ee7212adcafb3d92c
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-15 09:13:49 +0600

    explore slice, split, join, substring, trim method

---
+++ split-join-slice-trim-substring.js
@@ -0,0 +1,37 @@
+const string = ' In publishing and graphic design. Lorem ipsum is a placeholder text commonly used to demonstrate. the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. ';
+
+//* split()
+// console.log(string.split()) //? return an array of string
+// console.log(string.split('')) //? split every character
+// console.log(string.split(' ')) //? split every word
+// console.log(string.split('.'))
+
+//* slice()
+// console.log(string.slice(1, 5))
+// console.log(string.slice(-5, -1))
+
+
+//* substring()
+// console.log(string.substring(1, 5))
+
+//* trim()
+// console.log(string)
+// console.log(string.trim())
+
+//* trimStart()
+// console.log(string.trimStart())
+
+//* trimEnd()
+// console.log(string.trimEnd())
+
+
+//* join()
+const line = [
+    'In publishing and graphic design',
+    'Lorem ipsum is a placeholder text',
+    'the visual form of a document or ',
+    'Lorem ipsum may'
+];
+// console.log(line.join('. '))
+
+//* 
